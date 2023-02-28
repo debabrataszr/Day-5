090047d4ac53a243106ddab969ee05db35bb7e88
debabrata-97 <debabrataszr@gmail.com>
2023-02-28 20:50:01 +0530

    DAY 5

---
+++ src/com/bridgelabz/Division.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class Division {
+    public static void main(String[] args) {
+        int dividend = 100, divisor = 4;
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("Quotient = " + quotient);
+        System.out.println("Remainder = " + remainder);
+    }
+}
---
+++ src/com/bridgelabz/Harmonic_Number.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Harmonic_Number {
+    public static void main(String[] args) {
+        double N, i;
+        double sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        N = sc.nextDouble();
+        for (i = 1; i <= N; i++) {
+            sum = sum + (1 / i);
+        }
+        System.out.println("Harmonic value = " + sum);
+    }
+}
